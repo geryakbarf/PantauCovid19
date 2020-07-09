ade3a3f0bab7bc80af1086fe9d4e16c666ac3953
geryakbar <geryakbarfauzi@gmail.com>
2020-07-09 15:02:00 +0700

    Initial commit

---
+++ app/src/main/java/com/ergnologi/pantaucovid19/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ergnologi.pantaucovid19.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ergnologi.pantaucovid19.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
